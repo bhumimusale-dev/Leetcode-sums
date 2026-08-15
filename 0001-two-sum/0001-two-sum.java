08363762613a3292474a5fbb5a5fd54596b39fd1
Bhumi Musale <musalebhumi44@gmail.com>
2026-08-15 20:07:00 +0530

    Time: 3 ms (59.04%), Space: 47.3 MB (23.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,35 +1,41 @@
-// class Solution {
+
 //     public int[] twoSum(int[] nums, int target) {
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         for (int i = 0; i < nums.length; i++){
-//            int complement = target - nums{i];
+//         int left = 0;
+//         int right = arr.length-1;
 
-//         if (map.containsKey(complement)){
-//             return new int[]{map.get(complement), i};
+//         while (left < right){
+//             int sum = arr[left] + arr[right];
+
+//             if (sum == target){
+//                 System.out.println("Found");
+//             }
+//                 else if(sum > target){
+//                     right --
+//                 }
+//                     else {
+//                         left--
+//                     }
+//                 }
+//             }
 //         }
-
-//         map.put(nums[i], i);
+        
 //     }
-//       return new int[]{};
-//    }
 // }
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+class Solution{
+public int []twoSum(int[]nums, int target){
+    HashMap<Integer, Integer>map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
+    for(int i=0; i<nums.length; i++){
+        int complement = target - nums[i];
 
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
-            }
-
-            map.put(nums[i], i);
-        }
-
-        return new int[]{};
+        if (map.containsKey(complement)){
+        return new int[]{map.get(complement), i};
     }
+     map.put(nums[i], i);
+}
+
+return new int[]{};
+}
 }
